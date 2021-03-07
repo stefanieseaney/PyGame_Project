21d5ffba708b102f527541423be74424e6f12b1f
Stefanie Seaney <stefanie.seaney@gmail.com>
2021-03-07 17:47:07 -0600

    Increase and round score as game is played.

--- alien_invasion.py
+++ alien_invasion.py
@@ -71,6 +71,7 @@ class AlienInvasion:
             self.settings.initialize_dynamic_settings()
             self.stats.reset_stats()
             self.stats.game_active = True
+            self.sb.prep_score()
 
             # Get rid of any remaining aliens and bullets.
             self.aliens.empty()
@@ -123,6 +124,11 @@ class AlienInvasion:
         """Respond to bullet-alien collisions."""
         # Remove any bullets and aliens that have collided.
         collisions = pygame.sprite.groupcollide(self.bullets, self.aliens, True, True)
+
+        if collisions:
+            for aliens in collisions.values():
+                self.stats.score += self.settings.alien_points * len(aliens)
+            self.sb.prep_score()
 
         if not self.aliens:
             # Destroy exisiting bullets and create new fleet.
